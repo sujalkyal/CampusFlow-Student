9f96a42438f7c4e2a901bc8a53ad25a5146cdb85
sujalkyal <sujalkyal2704@gmail.com>
2025-04-20 18:25:07 +0530

    deploy-3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js or next.config.mjs
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Only apply the plugin on the server-side build
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
